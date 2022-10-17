d83f0275d349a3b20d1a31161267bb2cebdd78e3
ta5q-hr3 <51683531+tasuku-maedag@users.noreply.github.com>
2022-10-17 13:18:14 +0900

    sample04 modified

--- sample04/index.js
+++ sample04/index.js
@@ -27,3 +27,84 @@ gsap.from(
   }
 );
 
+
+/*
+Standard Stagger 12
+propaties : axis x
+*/
+const elmStaggerAxisX = document.querySelectorAll(".element__parent.axis_x .rect");
+
+gsap.from(
+  elmStaggerAxisX,
+  {
+    opacity:0,
+    //y:-20,
+    scale:0.1,
+    ease:"power4.out",
+    duration:1,
+    repeat:8,
+    repeatDelay:1,
+    stagger: {
+      each:0.1,
+      from:"center",
+      grid:"auto",
+      ease:"power4.out",
+
+      axis:"x"
+    }
+  }
+);
+
+
+/*
+Standard Stagger 13
+propaties : axis y
+*/
+const elmStaggerAxisY = document.querySelectorAll(".element__parent.axis_y .rect");
+
+gsap.from(
+  elmStaggerAxisY,
+  {
+    opacity:0,
+    //y:-20,
+    scale:0.1,
+    ease:"power4.out",
+    duration:1,
+    repeat:8,
+    repeatDelay:1,
+    stagger: {
+      each:0.1,
+      from:"center",
+      grid:"auto",
+      ease:"power4.out",
+
+      axis:"y"
+    }
+  }
+);
+
+
+/*
+Standard Stagger 14
+propaties : axis y
+*/
+const elmStaggerEase = document.querySelectorAll(".element__parent.ease .rect");
+
+gsap.from(
+  elmStaggerEase,
+  {
+    opacity:0,
+    //y:-20,
+    scale:0.1,
+    ease:"power4.out",
+    duration:1,
+    repeat:8,
+    repeatDelay:1,
+    stagger: {
+      each:0.1,
+      from:"center",
+      grid:"auto",
+      ease:"power4.out",
+    }
+  }
+);
